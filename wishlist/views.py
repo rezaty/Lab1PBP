987056e40028d7123da0625318b6ff2ca7b332e2
rezataufiq56 <rezataufiq56@gmail.com>
2022-09-14 20:50:59 +0700

    Tutorial Lab 2 PBP

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 def show_wishlist(request):
@@ -7,4 +9,21 @@ def show_wishlist(request):
     'nama': 'Reza Taufiq Yahya'
 }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json (request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id (request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id (request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
 
